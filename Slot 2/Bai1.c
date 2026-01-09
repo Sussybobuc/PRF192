4524d6bf4b3b726e43fdce105a6dfcd2bf8dc33d
Nhat Huy <itwasajokestop@gmail.com>
2026-01-09 10:48:51 +0700

    Bai 2

--- Slot 2/Bai1.c
+++ Slot 2/Bai1.c
@@ -1,1 +1,11 @@
 #include<stdio.h>
+int main() {
+	int Number;
+	int a;
+	a = 15;
+	int x = 10;
+	printf("Type number: ");
+	scanf("%d", &Number);
+	printf("\n %d %d %d ", a,x, Number );
+	return 0;
+}
